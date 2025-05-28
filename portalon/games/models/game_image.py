c370c03b0b300bd405184ed33fbbac0c48b03bbe
miguel <miguelsdl@gmail.com>
2025-05-28 19:37:15 -0300

    <

--- portalon/games/models/game_image.py
+++ portalon/games/models/game_image.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from game import Game
-from cms.models import CMSPlugin
 from filer.fields.image import FilerImageField
 
 
