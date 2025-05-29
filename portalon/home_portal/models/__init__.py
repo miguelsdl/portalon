2f027b76418ccdf11ab06e1a7cd035ed24817f08
miguel <miguelsdl@gmail.com>
2025-05-28 21:57:44 -0300

    .

---
+++ portalon/home_portal/models/__init__.py
@@ -0,0 +1,4 @@
+from games.models.game import Game
+from games.models.game_category import GameCategory
+from games.models.game_image import GameImage
+from games.models.game_platform import GamePlatform
